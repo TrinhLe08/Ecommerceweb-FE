2ba6a041944f4303d5e6beaf03d0ee1af4ebc1c1
TrinhLe08 <khanhtrinhmc2021@gmail.com>
2025-08-21 12:13:02 +0700

    update

--- src/app/component/home-page/page.tsx
+++ src/app/component/home-page/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect } from "react";
-import Link from "next/link";
 import { useRecoilState } from "recoil";
 import { AtomSidebaCheckUnderline } from "@/app/recoil/sidebar-check-provider";
 import { AtomReturnInformationWhenLogin } from "@/app/recoil/information-user-provider";
@@ -11,6 +10,11 @@ const HomePage = () => {
   const [__, setReturnInformation] = useRecoilState(
     AtomReturnInformationWhenLogin
   );
+  const urlTopicPhoto = "https://media.architecturaldigest.com/photos/5e6a42b25c94700009daa7d7/master/w_1920%2Cc_limit/AD0420_DIRAND_7.jpg"
+  const urlArtwork = "https://image.invaluable.com/housePhotos/Izsolu/44/596844/H20345-L105490608.jpg"
+  const urlHomeDecor = "https://media.designcafe.com/wp-content/uploads/2020/08/11193522/living-room-decor-ideas.jpg"
+  const urlKitchen = "https://www.leifshop.com/cdn/shop/files/shop.jpg?v=1718894153&width=1500"
+  const urlHoliday = "https://buchanansplants.com/wp-content/uploads/2021/11/E6r40w0w-scaled.jpeg"
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -25,108 +29,62 @@ const HomePage = () => {
     }
   }, []);
 
-  const firstMenu = [
-    {
-      url1: "https://images.saatchiart.com/saatchi/425969/art/8694258/7757825-WUECRQDY-7.jpg",
-      url2: "https://images.saatchiart.com/saatchi/307665/art/2901357/1971250-HSC00001-7.jpg",
-      title: "SHOP ORIGIN ARWORK",
-      link: "/?product-page=artwork",
-      checkSidebar: 2,
-    },
-    {
-      url1: "https://www.leifshop.com/cdn/shop/files/naninincense.jpg?v=1738726139&width=823",
-      url2: "https://www.leifshop.com/cdn/shop/products/tickingstripeframecopy.jpg?v=1679944880&width=823",
-      title: "SHOP HOME DECORD",
-      link: "/?product-page=home-decord",
-      checkSidebar: 1,
-    },
-  ];
 
-  const secondMenu = [
-    {
-      url1: "https://www.tillyliving.com/cdn/shop/products/2_686e478c-e9d1-4fa0-a10c-fa208df2a077.jpg?v=1655774105",
-      url2: "https://www.leifshop.com/cdn/shop/collections/serveware_480x480.jpg?v=1686689639",
-      title: "KITCHEN TOOLS",
-      link: "/?product-page=kitchen",
-      checkSidebar: 3,
-    },
-    {
-      url1: "https://static.vinwonders.com/production/Christmas-Gifts-4.jpg",
-      url2: "https://www.leifshop.com/cdn/shop/products/grilledcheeseornament_1000X1000.jpg?v=1664937165",
-      title: "HOLIDAY GIFTS",
-      link: "/?product-page=holiday",
-      checkSidebar: 4,
-    },
-    {
-      url1: "https://www.leifshop.com/cdn/shop/products/redcherriescard.jpg?v=1651006516&width=360",
-      url2: "https://www.leifshop.com/cdn/shop/files/jewelry_hp_1000x.jpg?v=1693169060",
-      title: "ITEMS IS ON SALE",
-      link: "/?product-page=sale",
-      checkSidebar: 5,
-    },
-  ];
+
   return (
-    <div className="home-page grid gap-5 h-fit w-[70%] border-t border-gray-300 p-4">
-      <div className="w-full h-full first-menu lg:flex grid gap-5 justify-around">
-        {
-          firstMenu.map((menu) => (
-            <div
-              key={menu.title}
-              className="grid relative group"
-            >
-              <div className=" xl:w-[500px] xl:h-[700px] lg:w-[500px] lg:h-[700px] md:w-[500px] md:h-[600px] w-[300px] h-[400px]">
-                <img
-                  src={menu.url1}
-                  alt=""
-                  className="xl:ml-0 xl:w-[500px] xl:h-[700px] lg:ml-0 lg:w-[500px] lg:h-[700px] md:ml-0 md:w-[500px] md:h-[600px] ml-[-50px] w-[300px] h-[400px] absolute top-0 left-0 transition-opacity duration-500 group-hover:opacity-0"
-                />
-                <img
-                  src={menu.url2}
-                  alt=""
-                  className="xl:ml-0 xl:w-[500px] xl:h-[700px] lg:ml-0 lg:w-[500px] lg:h-[700px] md:ml-0 md:w-[500px] md:h-[600px] ml-[-50px] w-[300px] h-[400px] absolute top-0 left-0 transition-opacity duration-500 opacity-0 group-hover:opacity-100"
-                />
-              </div>
-              <Link
-                href={menu.link}
-                onClick={() => setCheckSidebar(menu.checkSidebar)}
-                className="flex w-full text-xl justify-center mt-1 underline"
-              >
-                {menu.title}
-              </Link>
-            </div>
-          ))
-        }
+    <div className="home-page  font-serif  grid gap-5 h-fit w-[80%] border-t border-gray-300 p-4">
+      <div className="w-full h-full first-menu lg:grid grid gap-5 justify-around">
+        <img
+          className="w-[100%]"
+          src={urlTopicPhoto}
+          alt=""
+        />
+        <p className="text-center font-bold text-2xl">
+          Leif's aesthetic is modern and minimalist, yet exudes warmth and a distinct spirit.
+          We champion natural materials (wood, stone, ceramic, glass, ...),
+          employ a gentle neutral color palette, and accent with geometric or nature-inspired patterns.
+        </p>
       </div>
-      <div className="second-menu lg:flex grid justify-around">
-        {
-          secondMenu.map((menu) => (
-            <div
-              key={menu.title}
-              className="grid relative group"
-            >
-              <div className="w-[330px] h-[450px]">
-                <img
-                  src={menu.url1}
-                  alt={`Hình ảnh ${menu.title}`}
-                  className="w-[330px] h-[450px] absolute top-0 left-0 transition-opacity duration-500 group-hover:opacity-0"
-                />
-                <img
-                  src={menu.url2}
-                  alt={`Hình ảnh hover ${menu.title}`}
-                  className="w-[330px] h-[450px] absolute top-0 left-0 transition-opacity duration-500 opacity-0 group-hover:opacity-100"
-                />
-              </div>
-              <Link
-                href={menu.link}
-                onClick={() => setCheckSidebar(menu.checkSidebar)}
-                className="flex w-full text-xl justify-center mt-1 underline z-10 relative" // Thêm z-10 và relative để tránh bị đè lên
-              >
-                {menu.title}
-              </Link>
-            </div>
-          ))
-        }
+      <div className="second-menu lg:grid grid justify-around  border-t-2 border-slate-200 p-5 gap-7">
+        <p className="text-center font-bold">
+          Our products are meticulously curated to offer a truly distinct experience.
+          Rather than commonplace items, you will discover unique art pieces, exquisite handcrafted ceramics, sculptural vases that are artworks in their own right, elegant stationery, and refined gifts for every occasion.
+          Each item holds its own narrative and bears the unique imprint of its creator.
+        </p>
+        <div className="flex gap-5">
+          <img className="w-[30%]" src={urlArtwork} alt="" />
+          <p className="w-full h-full flex justify-center items-center text-center">
+            In our space, every piece is a testament to pure creativity.
+            We are not merely a store, but a curated gallery—where we are passionately committed to serving as a steadfast and inspiring bridge,
+            connecting the hearts of art enthusiasts with the hands and minds of talented independent artists and designers.
+          </p>
+        </div>
+        <div className="flex gap-5">
+          <p className="w-full h-full flex justify-center items-center text-center">
+            Going beyond ordinary items, our collection of home decor features unique artistic pieces, carefully selected to breathe soul into your space.
+            Each item is not merely a decoration, but a refined accent that helps craft an ideal home—a sanctuary brimming with warmth and personal style.
+          </p>
+          <img className="w-[40%]" src={urlHomeDecor} alt="" />
+        </div>
+        <div className="flex gap-5">
+          <img className="w-[30%]" src={urlKitchen} alt="Kitchenware" />
+          <p className="w-full h-full flex justify-center items-center text-center">
+            Discover our kitchenware collection—where exquisite craftsmanship meets flawless utility.
+            Each piece is not just a reliable kitchen aid but also a decorative work of art,
+            elevating your cooking space to new heights of sophistication and inspired elegance.
+          </p>
+        </div>
+        <div className="flex gap-5">
+          <p className="w-full h-full flex justify-center items-center text-center">
+            Transform every special moment into a sparkling memory with our festive decoration collection.
+            From meaningful, hand-picked gifts that touch the recipient's heart to decorative items that fill your living space with the vibrant colors and
+            joy of the season.
+          </p>
+          <img className="w-[35%]" src={urlHoliday} alt="" />
+        </div>
       </div>
+
+
       <div className="w-full grid justify-center">
         <div className="h-fit grid gap-5 italic font-serif lg:w-[800px] w[30%] text-center my-10">
           <div className="text-xl font-semibold">THE LEIF LIFESTYLE.</div>
--- src/app/component/master-playout/footer/page.tsx
+++ src/app/component/master-playout/footer/page.tsx
@@ -9,44 +9,46 @@ interface CustomNotificationProps {
 
 const Footer = () => {
   return (
-    <div className="hidden md:grid footer font-serif w-screen  gap-5 text-center justify-center border-t-2 border-slate-200 p-5">
-      <div className="grid w-[900px] gap-9 justify-center">
-        <div>
-          <h1 className="font-sans font-semibold">#LEIFLIFESTYLE</h1>
-          <p className="text-xl italic">goods for living</p>
-        </div>
-
-        <div className="flex justify-center gap-9 ">
-          <div className="grid border-r-2 border-slate-200 p-5">
-            <p>WILLIAMSBURG, BROOKLYN</p>
-            <p>99 GRAND STREET</p>
-            <p>BROOKLYN, NY 11249</p>
+    <div className="hidden md:grid footer font-serif  w-screen grid justify-center gap-5 text-center justify-center border-t-2 border-slate-200 p-5">
+      <div className="w-fit grid gap-5">
+        <div className="grid w-[900px] gap-9 justify-center">
+          <div>
+            <h1 className="font-sans font-semibold">#LEIFLIFESTYLE</h1>
+            <p className="text-xl italic">goods for living</p>
           </div>
 
-          <img
-            className="w-[20%]"
-            src="https://www.leifshop.com/cdn/shop/t/49/assets/leif_stamp2.svg?v=119490858498548638031643027686"
-            alt=""
-          />
+          <div className="flex justify-center gap-9 ">
+            <div className="grid border-r-2 border-slate-200 p-5">
+              <p>WILLIAMSBURG, BROOKLYN</p>
+              <p>99 GRAND STREET</p>
+              <p>BROOKLYN, NY 11249</p>
+            </div>
 
-          <div className="grid border-l-2 border-slate-200 p-5">
-            <h1>SAY HI :</h1>
-            <p>FOLLOW US @LEIFSHOP</p>
-            <p>HELLO@LEIFSHOP.COM</p>
+            <img
+              className="w-[20%]"
+              src="https://www.leifshop.com/cdn/shop/t/49/assets/leif_stamp2.svg?v=119490858498548638031643027686"
+              alt=""
+            />
+
+            <div className="grid border-l-2 border-slate-200 p-5">
+              <h1>SAY HI :</h1>
+              <p>FOLLOW US @LEIFSHOP</p>
+              <p>HELLO@LEIFSHOP.COM</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex gap-5 justify-center">
-        <div>© 2024 LEIF, ALL RIGHTS RESERVED</div>
-        <div className="flex gap-2 italic">
-          follow us
-          <a href="http://pinterest.com/leifshop/">
-            <AiFillPinterest size={30} style={{ color: "lightcoral" }} />
-          </a>
-          <a href="http://www.instagram.com/leifshop">
-            <FaInstagramSquare size={30} style={{ color: "lightcoral" }} />
-          </a>
+        <div className="flex gap-5 justify-center">
+          <div>© 2024 LEIF, ALL RIGHTS RESERVED</div>
+          <div className="flex gap-2 italic">
+            follow us
+            <a href="http://pinterest.com/leifshop/">
+              <AiFillPinterest size={30} style={{ color: "lightcoral" }} />
+            </a>
+            <a href="http://www.instagram.com/leifshop">
+              <FaInstagramSquare size={30} style={{ color: "lightcoral" }} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
